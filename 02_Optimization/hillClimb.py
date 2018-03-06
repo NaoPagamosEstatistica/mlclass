b29d5e4056da2ebc0ba33fa07ef5e37a050c27d9
NelsonGomesNeto <ngn@ic.ufal.br>
2018-03-06 14:41:55 -0300

    Implemented simple Hill Climbing

--- 02_Optimization/hillClimb.py
+++ 02_Optimization/hillClimb.py
@@ -1,9 +1,45 @@
 import os
 import subprocess
+import struct
+import sys
 from random import randint
+inf = 1<<20
 os.system("g++ hillClimb.cpp -o test")
 
-args = [90, 90, 90, 90, 90, 90]
-args = list(map(str, args))
-result = subprocess.check_output(["./test", *args])
-print(result)
+def neighbors(node, r):
+    nb = [node]
+    for i in range(10):
+        newNode = []
+        for j in range(6):
+            newNode += [(node[j] + randint(-r, r) + 360) % 360]
+        nb += [newNode]
+    return(nb)
+
+def evaluate(x):
+    x = list(map(str, x))
+    result = subprocess.check_output(["./test", *x])
+    s = str(result)[2:len(result) - 3]
+    f = float(s)
+    return(f)
+
+def hillClimb(startNode, r):
+    currNode = startNode
+    while (True):
+        nb = neighbors(currNode, r)
+        nextEval, nextNode = -inf, 0
+        for x in nb:
+            result = evaluate(x)
+            if (result > nextEval):
+                nextNode = x
+                nextEval = result
+        print("Best so far:", nextNode, nextEval)
+        if (nextEval <= evaluate(currNode)):
+            return(currNode)
+        currNode = nextNode
+
+arguments = sys.argv
+r = int(arguments[1])
+startNode = [90, 90, 90, 90, 90, 90]
+print(startNode, evaluate(startNode))
+bestNode = hillClimb(startNode, r)
+print(bestNode, evaluate(bestNode))
