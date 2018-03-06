3b4ef0d6b6b01fd42d124c7861fbf7cfd8748836
NelsonGomesNeto <ngn@ic.ufal.br>
2018-03-06 18:07:58 -0300

    Implementing simple Hill Climb in C++

--- 02_Optimization/hillClimb.cpp
+++ 02_Optimization/hillClimb.cpp
@@ -1,4 +1,25 @@
 #include <bits/stdc++.h>
+#include "shoroShiro.h"
+using namespace std;
+int DEBUG = 0, kNeighbors, radious, kStuck;
+vector<vector<int> > nb; vector<int> aux = {0, 0, 0, 0, 0, 0};
+
+int randInt()
+{
+	int rr = (next() % (2*radious + 1)) - radious;
+	return(rr);
+}
+
+void neighbors(vector<int> node)
+{
+	nb.clear();
+	for (int i = 0; i < kNeighbors; i ++)
+	{
+		for (int j = 0; j < 6; j ++)
+			aux[j] = (node[j] + randInt() + 360) % 360;
+		nb.push_back(aux);
+	}
+}
 
 double norm(int x)
 {
@@ -26,17 +47,58 @@ double simulate(int phi[], int theta[])
 	return(gg);
 }
 
+int gain(vector<int> x)
+{
+	int phi[3] = {x[0], x[2], x[4]};
+	int theta[3] = {x[1], x[3], x[5]};
+	return(simulate(phi, theta));
+}
+
+void hillClimb(vector<int> startNode)
+{
+	vector<int> currNode = startNode; int stuck = 0;
+	while (1)
+	{
+		neighbors(currNode);
+		int nextEval = -(1 << 20);
+		vector<int> nextNode = aux;
+		for (auto x: nb)
+		{
+			int result = gain(x);
+			if (result > nextEval)
+			{
+				nextNode = x;
+				nextEval = result;
+			}
+		}
+		if (DEBUG) printf("Now: %d, Best: %d\n", nextEval, gain(currNode));
+		if (nextEval <= gain(currNode))
+			stuck ++;
+		else
+		{
+			stuck = 0;
+			currNode = nextNode;
+		}
+		if (stuck == kStuck)
+			break;
+	}
+}
+
 int main(int argc, char **argv)
 {
-	int phi[3], theta[3];
+	s[0] = time(NULL);
+	s[1] = time(NULL); rotl(2, 3); next(); next(); next();
+	int phi[3], theta[3]; vector<int> start;
 	for (int i = 0; i < 3; i ++)
 	{
 		phi[i] = atoi(argv[2*i + 1]);
 		theta[i] = atoi(argv[2*i + 2]);
 		//scanf("%d %d", &phi[i], &theta[i]);
+		//printf("%d %d\n", phi[i], theta[i]);
 	}
 	double result = simulate(phi, theta);
-	printf("%.20lg\n", result);
-
+	start = {phi[0], theta[0], phi[1], theta[1], phi[2], theta[2]};
+	hillClimb(start);
+	
 	return(0);
 }
--- 02_Optimization/hillClimb.py
+++ 02_Optimization/hillClimb.py
@@ -2,44 +2,81 @@ import os
 import subprocess
 import struct
 import sys
+import numpy as np
 from random import randint
+from math import cos, sin, exp, sqrt
+DEBUG = 0
+kNeighbors, radious, kStuck = 0, 0, 0
 inf = 1<<20
-os.system("g++ hillClimb.cpp -o test")
+#print(os.system("g++ hillClimb.cpp -o test"))
 
-def neighbors(node, r):
-    nb = [node]
-    for i in range(10):
+def norm(x):
+    return(x / (36.0) - 5.0)
+
+def gain(param):
+    x = norm(param[0])
+    y = norm(param[1])
+    gain1 = sin(x) + cos(y)
+
+    x = norm(param[2])
+    y = norm(param[3])
+    gain2 = y * sin(x) - x * cos(y)
+
+    x = norm(param[4])
+    y = norm(param[5])
+    r = sqrt(x * x + y * y)
+    gain3 = sin(x * x + 3.0 * y * y) / (0.1 + r * r) + (x * x + 5.0 * y * y) * exp(1.0 - r * r) / 2.0
+
+    return(4.0 * gain1 + gain2 + 4.0 * gain3)
+
+
+def neighbors(node):
+    nb = []
+    for i in range(kNeighbors):
         newNode = []
         for j in range(6):
-            newNode += [(node[j] + randint(-r, r) + 360) % 360]
+            newNode += [(node[j] + randint(-radious, radious) + 360) % 360]
         nb += [newNode]
     return(nb)
 
-def evaluate(x):
-    x = list(map(str, x))
-    result = subprocess.check_output(["./test", *x])
-    s = str(result)[2:len(result) - 3]
-    f = float(s)
-    return(f)
+# def gain(x):
+#     return(gain(x))
+#     x = list(map(str, x))
+#     result = subprocess.check_output(["./test", *x])
+#     lol = result
+#     s = lol[:len(lol) - 1]
+#     f = np.float64(s)
+#     #print(result, s, f, "%.20lf" % f)
+#     return(f)
 
-def hillClimb(startNode, r):
-    currNode = startNode
+def hillClimb(startNode):
+    currNode, stuck = startNode, 0
     while (True):
-        nb = neighbors(currNode, r)
+        nb = neighbors(currNode)
         nextEval, nextNode = -inf, 0
         for x in nb:
-            result = evaluate(x)
+            result = gain(x)
             if (result > nextEval):
                 nextNode = x
                 nextEval = result
-        print("Best so far:", nextNode, nextEval)
-        if (nextEval <= evaluate(currNode)):
+        if (DEBUG): print("Now:", nextNode, nextEval, "stuck:", stuck, "Best:", gain(currNode))
+        if (nextEval <= gain(currNode)):
+            stuck += 1
+        else:
+            stuck = 0
+            currNode = nextNode
+        if (stuck == kStuck):
             return(currNode)
-        currNode = nextNode
 
 arguments = sys.argv
-r = int(arguments[1])
-startNode = [90, 90, 90, 90, 90, 90]
-print(startNode, evaluate(startNode))
-bestNode = hillClimb(startNode, r)
-print(bestNode, evaluate(bestNode))
+DEBUG = int(arguments[1])
+radious = int(arguments[2])
+kNeighbors = int(arguments[3])
+kStuck = int(arguments[4])
+#startNode = [242, 180, 2, 334, 179, 156]
+#startNode = [180, 180, 180, 180, 180, 180]
+#startNode = [90, 90, 90, 90, 90, 90]
+startNode = list(map(int, arguments[5:]))
+print("Starting Hill Climb with:", startNode, gain(startNode), "radious =", radious, "kNeighbors =", kNeighbors, "kStuck =", kStuck)
+bestNode = hillClimb(startNode)
+print(bestNode, gain(bestNode))
