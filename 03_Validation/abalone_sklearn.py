610fdc96913a0a991bc30182e6bb315e3cec8800
NelsonGomesNeto <ngn@ic.ufal.br>
2018-04-09 01:07:00 -0300

    Ok... It seems like the submissions are blocked .-.

--- 03_Validation/abalone_sklearn.py
+++ 03_Validation/abalone_sklearn.py
@@ -1,12 +1,9 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 import pandas as pd
 from sklearn.neighbors import KNeighborsClassifier, NearestCentroid
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split, cross_val_score, cross_validate, cross_val_predict
-from sklearn import metrics, datasets
+from sklearn import metrics, datasets, preprocessing, linear_model, tree, naive_bayes, svm, neural_network
 from sklearn.metrics import recall_score
-SEND = 0
 
 def printScore(score, X, y_pred):
     for i in sorted(score):
@@ -15,17 +12,17 @@ def printScore(score, X, y_pred):
 
 print('\n - Lendo o arquivo com o dataset sobre diabetes')
 data = pd.read_csv('abalone_dataset_sexAsNum.csv')
-dataCopy = pd.read_csv('abalone_dataset_sexAsNumCopy.csv')
 
 print(' - Criando X e y para o algoritmo de aprendizagem a partir do arquivo diabetes_dataset')
 feature_cols = ['sex', 'length', 'diameter', 'height',
                 'whole_weight', 'shucked_weight', 'viscera_weight', 'shell_weight']
 X = data[feature_cols]
+#X = preprocessing.MinMaxScaler().fit_transform(X)
 y = data.type
 
 print(' - Criando modelo preditivo')
-model = [KNeighborsClassifier(n_neighbors=15), LinearRegression(fit_intercept=False), NearestCentroid()]
-toDo = [1, 0, 1]
+model = [KNeighborsClassifier(n_neighbors=19), LinearRegression(fit_intercept=False), NearestCentroid(), linear_model.SGDClassifier(loss="hinge", penalty="l2"), tree.DecisionTreeClassifier(), naive_bayes.GaussianNB(), svm.LinearSVC(), neural_network.MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(5,2), random_state=1)]
+toDo = [1, 0, 0, 0, 0, 0, 1, 1]
 
 scoring = ['precision_macro', 'recall_macro']
 for i in range(len(model)):
@@ -36,3 +33,4 @@ for i in range(len(model)):
 
     score_predict = cross_val_predict(model[i], X, y, cv=10)
     printScore(scores, y, score_predict)
+# 19
--- 03_Validation/abalone_submit.py
+++ 03_Validation/abalone_submit.py
@@ -1,12 +1,4 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-SEND = 0
-
-def printScore(score, X, y_pred):
-    print(" - Score:")
-    for i in sorted(score):
-        print("    ", i, ": ", max(score[i]), ", ", score[i], sep='')
-    print("    accuracy_score: ", metrics.accuracy_score(X, y_pred), sep='')
+SEND = 1
 
 """
 Atividade para trabalhar o pré-processamento dos dados.
@@ -27,39 +19,31 @@ import requests
 
 print('\n - Lendo o arquivo com o dataset sobre diabetes')
 data = pd.read_csv('abalone_dataset_sexAsNum.csv')
-dataCopy = pd.read_csv('abalone_dataset_sexAsNumCopy.csv')
 
 # Criando X and y par ao algorítmo de aprendizagem de máquina.
 print(' - Criando X e y para o algoritmo de aprendizagem a partir do arquivo diabetes_dataset')
 # Caso queira modificar as colunas consideradas basta algera o array a seguir.
 feature_cols = ['sex', 'length', 'diameter', 'height',
                 'whole_weight', 'shucked_weight', 'viscera_weight', 'shell_weight']
-#feature_cols = ['Pregnancies', 'Glucose', 'BloodPressure', 'SkinThickness',
-                #'Insulin', 'BMI', 'DiabetesPedigreeFunction', 'Age']
 X = data[feature_cols]
 y = data.type
 
 # Ciando o modelo preditivo para a base trabalhada
 print(' - Criando modelo preditivo')
-neigh = KNeighborsClassifier(n_neighbors=3)
-neigh.fit(X, y)
+model = KNeighborsClassifier(n_neighbors=19)
+model.fit(X, y)
 
 #realizando previsões com o arquivo de
 print(' - Aplicando modelo e enviando para o servidor')
 data_app = pd.read_csv('abalone_app_sexAsNum.csv')
-y_pred = neigh.predict(data_app)
-#print(*y_pred)
-
-scoring = ['precision_macro', 'recall_macro']
-scores = cross_validate(neigh, X, y, scoring=scoring, cv=5, return_train_score=True)
-score_predict = cross_val_predict(neigh, X, y, cv=5)
-printScore(scores, y, score_predict)
+y_pred = model.predict(data_app)
+print(pd.Series(y_pred).to_json(orient='values'))
 
 # Enviando previsões realizadas com o modelo para o servidor
 URL = "http://aydanomachado.com/mlclass/03_Validation.php"
 
 #TODO Substituir pela sua chave aqui
-DEV_KEY = "Não pagamos || estatística ||"
+DEV_KEY = "Não pagamos estatística"
 
 # json para ser enviado para o servidor
 data = {'dev_key':DEV_KEY,
