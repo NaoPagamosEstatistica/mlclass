d47a003edda368959a78cf207e66e77a80e6f5b3
NelsonGomesNeto <ngn@ic.ufal.br>
2018-05-23 11:37:24 -0300

    Finished V^pi estimation; NICE

--- Reinforcement Learning/Monty Hall/montyHallMonteCarlo.py
+++ Reinforcement Learning/Monty Hall/montyHallMonteCarlo.py
@@ -1,8 +1,14 @@
 from random import randint, shuffle
+doorsQuantity = 3
+montyChooses = 1
+episodes = 10000
+CHANGE = 1
+KEEP = 0
+WINNER_DOOR = 1
 
 def genDoor(doorsQuantity):
     door = [0] * doorsQuantity
-    door[randint(0, doorsQuantity - 1)] = 1
+    door[randint(0, doorsQuantity - 1)] = WINNER_DOOR
     return(door)
 
 def argMax(Q):
@@ -12,55 +18,86 @@ def argMax(Q):
             best = [i, Q[i]]
     return(best[0])
 
-pi = randint(0, 1) # policy start randomly
-doorsQuantity = 100
-montyChooses = 98
-e = 0.8 # learning rate
-Q = [0, 0] # there's only one state, which has two actions: change or not
-episodes = 10000
+def learnBestPolicy():
+    global doorsQuantity, montyChooses, episodes
+    pi = randint(KEEP, CHANGE) # policy start randomly
+    Q = [0, 0] # there's only one state, which has two actions: change or not
 
-ee = episodes
-while (ee):
+    ee = episodes
+    while (ee):
+        if (pi == CHANGE):
+            print("Policy: change")
+        else:
+            print("Policy: keep")
+
+        door = genDoor(doorsQuantity) # generating episode
+        print("Doors:", door)
+
+        available = list(range(doorsQuantity))
+        chosen = randint(0, doorsQuantity - 1)
+        available.remove(chosen) # choosing a door
+
+        aux = montyChooses
+        while (aux):
+            while (door[available[0]]): # monty can't choose the winner door
+                shuffle(available)
+            available.remove(available[0]) # monty chooses a goat
+            aux -= 1
+
+        final = chosen
+        if (pi == CHANGE): # if policy says to change
+            shuffle(available)
+            final = available[0]
+
+        if (door[final]):
+            print("Winner")
+        else:
+            print("Loser")
+
+        Q[KEEP], Q[CHANGE] = Q[KEEP] + door[chosen] / episodes, Q[CHANGE] + door[available[0]] / episodes # saving ""mean"" of returns
+
+        pi = argMax(Q) # getting new policy through argMax; since there's only one state, it will just choose between action of that single state
+
+        print("Q: [keep: %lf, change: %lf]" % (Q[KEEP], Q[CHANGE]), end='\n\n')
+        ee -= 1
+
     if (pi):
-        print("Policy: change")
+        print("Best policy: Change")
     else:
-        print("Policy: keep")
+        print("Best policy: Keep")
+    return(pi)
 
-    door = genDoor(doorsQuantity) # generating episode
-    print("Doors:", door)
-
-    available = list(range(doorsQuantity))
-    chosen = randint(0, doorsQuantity - 1)
-    available.remove(chosen) # choosing a door
-    print(chosen, available)
-
-    aux = montyChooses
-    while (aux):
-        monty = randint(0, doorsQuantity - 1)
-        while (door[monty] or monty == chosen or monty not in available): # can't choose winner door or chosen door
-            monty = randint(0, doorsQuantity - 1)
-        available.remove(monty) # monty chooses a goat
-        aux -= 1
-    print(available)
-
-    final = chosen
-    if (pi): # if policy says to change
-        shuffle(available)
-        final = available[0]
-
+def evaluatePolicy(pi):
+    global doorsQuantity, montyChooses, episodes
     won = 0
-    if (door[final]):
-        print("Winner")
-        won = 1
-    else:
-        print("Loser")
 
-    Q[0], Q[1] = Q[0] + door[chosen] / episodes, Q[1] + door[available[0]] / episodes # saving ""mean"" of returns
+    ee = episodes
+    while (ee):
+        door = genDoor(doorsQuantity) # generating episode
 
-    pi = argMax(Q) # getting new policy through argMax; since there's only one state, it will just choose between action of that single state
+        available = list(range(doorsQuantity))
+        chosen = randint(0, doorsQuantity - 1)
+        available.remove(chosen) # choosing a door
 
-    ee -= 1
-    print("Q: [keep: %lf, change: %lf]" % (Q[0], Q[1]), end='\n\n')
+        aux = montyChooses
+        while (aux):
+            while (door[available[0]]): # monty can't choose the winner door
+                shuffle(available)
+            available.remove(available[0]) # monty chooses a goat
+            aux -= 1
 
-print("Q: [keep: %lf, change: %lf]" % (Q[0], Q[1]))
-print("Expected probability:", (doorsQuantity - 1) / (doorsQuantity * (doorsQuantity - montyChooses - 1)))
+        final = chosen
+        if (pi): # if policy says to change
+            shuffle(available)
+            final = available[0]
+
+        if (door[final]):
+            won += 1
+
+        ee -= 1
+    return(won / episodes)
+
+learnBestPolicy()
+print("Change Theoretical Expectation:", (doorsQuantity - 1) / (doorsQuantity * (doorsQuantity - montyChooses - 1)))
+print("Keep-policy Expectation       :", evaluatePolicy(KEEP))
+print("Change-policy Expectation     :", evaluatePolicy(CHANGE))
